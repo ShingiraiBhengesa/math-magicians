db82a2b408a59a923f9eeb03ff92c404221ee347
ShingiraiBhengesa <72065045+ShingiraiBhengesa@users.noreply.github.com>
2022-05-25 18:04:21 +0300

    upadate changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
